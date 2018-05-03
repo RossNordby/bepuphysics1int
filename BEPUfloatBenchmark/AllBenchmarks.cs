acfd2b91014fc47774faa567ddeaee197ef21fb2
AlastairGraham <toasting@gmail.com>
2018-05-03 20:39:47 +0100

    Compiling within a Unity project.

--- BEPUfloatBenchmark/AllBenchmarks.cs
+++
@@ -1,9 +0,0 @@
-﻿using BEPUfloatBenchmark.Benchmarks;
-
-namespace BEPUfloatBenchmark
-{
-	public static class AllBenchmarks
-	{
-		public static Benchmark[] Benchmarks = { new DiscreteVsContinuousBenchmark(), new InverseKinematicsBenchmark(), new PathFollowingBenchmark(), new SelfCollidingClothBenchmark(), new PyramidBenchmark() };
-	}
-}
